50d7b04e5e8a2c93b1edf40ee7888d4c40e7aa79
kaif-999 <kaif@pentagoat.com>
2026-01-26 22:27:39 +0530

    adding nav code

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState }  from 'react';
 import {navbarStyles} from '../assets/dummyStyles.js';
 import { Clock } from 'lucide-react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
 
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "Watches", href: "/watches" },
+  { name: "Contact", href: "/contact" },
+];
 
 const Navbar = () => {
 
+const [open, setOpen] = useState(false);
+const location = useLocation();
+const navigate = useNavigate();
 
+const [active, setActive] = useState(location.pathname || "/");
+
+const handleNavClick = (href) => {
+
+    setActive(href);
+    setOpen(false);
+};
     return (
 <header className={navbarStyles.header}>
     <nav className={navbarStyles.nav} role='navigation'>
@@ -27,7 +42,32 @@ ChronoElite
 </Link>
 
     </div>
+{
 
+    //Desktop Navigation
+
+}
+
+<div className={navbarStyles.desktopNav}>
+
+{navItems.map((item)=> { const isActive = active ===  item.href;
+
+return (
+
+    <Link key={item.name} to ={item.href} onClick={() => handleNavClick(item.href)} className={`${navbarStyles.navItemBase} ${isActive ? navbarStyles.navItemActive: navbarStyles.navItemInactive}`}>
+<span>{item.name}</span>
+
+<span className={`${navbarStyles.activeIndicator} ${
+isActive ? navbarStyles.activeIndicatorVisible : navbarStyles.activeIndicatorHidden
+}`}></span>
+
+    </Link>
+)
+
+
+
+}  )}
+</div>
 
 </div>
 
@@ -42,4 +82,4 @@ ChronoElite
     )
 }
 
-export default Navbar
+export default Navbar;
